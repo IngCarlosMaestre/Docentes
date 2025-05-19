55c61844209dd8d8e124d15087c167d9ffe8f2f2
Longern1 <carlosa-maestreg@unilibre.edu.co>
2025-05-18 22:06:32 -0500

    corregido17

--- app.py
+++ app.py
@@ -27,19 +27,30 @@ def buscar_docente():
     query = request.args.get('q', '').lower()
     conn = conectar()
     cursor = conn.cursor(dictionary=True)
-    
+
     if query:
         cursor.execute("""
-            SELECT * FROM Docentes 
-            WHERE LOWER(nombre) LIKE %s OR LOWER(materias) LIKE %s
+            SELECT D.*, 
+                ROUND(AVG(R.valor_calificacion), 1) AS promedio
+            FROM Docentes D
+            LEFT JOIN Resenas R ON D.id_docente = R.id_docente
+            WHERE LOWER(D.nombre) LIKE %s OR LOWER(D.materias) LIKE %s
+            GROUP BY D.id_docente
         """, ('%' + query + '%', '%' + query + '%'))
     else:
-        cursor.execute("SELECT * FROM Docentes")
+        cursor.execute("""
+            SELECT D.*, 
+                ROUND(AVG(R.valor_calificacion), 1) AS promedio
+            FROM Docentes D
+            LEFT JOIN Resenas R ON D.id_docente = R.id_docente
+            GROUP BY D.id_docente
+        """)
     
     docentes = cursor.fetchall()
     conn.close()
 
     return jsonify(docentes)
+
 
 import traceback
 
